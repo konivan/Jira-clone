e6c443f5549568fe933ef6abc1c2efd06a77cadc
konivan <konivan08@gmail.com>
2022-10-26 15:09:46 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import RootStore from './store';
+
+const store = RootStore.create({});
+
+const StoreContent = createContext(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StoreContent.Provider value={store}>
+      <App />
+    </StoreContent.Provider>
   </React.StrictMode>
 );
